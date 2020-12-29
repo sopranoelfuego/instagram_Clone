092a968d58146726d6f47f6f68aac1bc88cfdffb
soprano <aubinjaja@gmail.com>
2020-12-29 18:03:05 +0200

    updates

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,15 +1,7 @@
 import firebase from "firebase"
+import config from "./config/firebaseCredit"
 
-
-const firebaseConfig=firebase.initializeApp({
-    apiKey: "AIzaSyDeA-x-u-mxlugxo6s--8KKId_CUuSk2MY",
-    authDomain: "instagramclone-255f3.firebaseapp.com",
-    databaseURL: "https://instagramclone-255f3.firebaseio.com",
-    projectId: "instagramclone-255f3",
-    storageBucket: "instagramclone-255f3.appspot.com",
-    messagingSenderId: "124543206714",
-    appId: "1:124543206714:web:36099453712ba79bf03c03"
-  })
+const firebaseConfig=firebase.initializeApp(config)
 
   const db=firebaseConfig.firestore()
   const auth=firebaseConfig.auth()
